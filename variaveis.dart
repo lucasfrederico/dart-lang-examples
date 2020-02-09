0aa7643cbce51b975c3e2ed2eaaa6f422c193ce9
lucasfrederico <lucas.frederico@outlook.com.br>
2020-02-09 10:39:27 -0300

    Variáveis

---
+++ variaveis.dart
@@ -0,0 +1,16 @@
+void main() {
+
+  // Implementando uma lojinha
+
+  String nome = "Lojinha do Dart";
+  int bananas = 5;
+  double preco = 1.5;
+  bool aindaTem = true;
+
+  print("O nome da Lojinha é: $nome");
+  print("A $nome tem $bananas bananas");
+
+  dynamic var1 = 10; // Sem tipo
+  var var2 = 10; // Pega o tipo do valor
+
+}
