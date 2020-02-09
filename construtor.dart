df244c80275a8092ae36651c91cb9e17459c501b
lucasfrederico <lucas.frederico@outlook.com.br>
2020-02-09 11:01:35 -0300

    Getter e Setter

--- construtor.dart
+++ construtor.dart
@@ -1,7 +1,18 @@
 class Pessoa {
   String nome;
-  int idade;
-  double altura;
+  int _idade;
+  double _altura;
 
-  Pessoa(this.nome, this.idade, this.altura);
+  Pessoa(this.nome, this._idade, this._altura);
+
+  int get idade {
+    return _idade;
+  }
+
+  // Utilizar o setter somente quando precisa proteger a definição com alguma  condição
+  set altura(double altura) {
+    if (altura > 0.0 && altura < 3.0) {
+      _altura = altura;
+    }
+  }
 }
